81b71058bd9f738ffb5d5c0e32e886c204503c5f
Thomas Herzog <herzog.thomas81@gmail.com>
2015-11-21 11:16:48 +0200

    - lab 6

---
+++ 20151121_lab_6/SWK5.MediaAnnotator/SWK5.MediaAnnotator/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace Swk5.MediaAnnotator
+{
+    public static class Constants
+    {
+        // for what 'http:/1drv.ms/1Mk01GV'
+        public const int MaxPreviewItems = 4;
+        public static readonly string[] MediaExt = { ".gif", ".jpg" };
+        public static readonly string BaseMediaFolder = @"C:\Users\Thomas\Git Repositories\Github\FH-Hagenberg\C-Sharp\20151121_lab_6\SamplePictures";
+    }
+}
---
+++ 20151121_lab_6/SWK5.MediaAnnotator/SWK5.MediaAnnotator/ViewModels/MediaItemVM.cs
@@ -0,0 +1,63 @@
+﻿using Swk5.MediaAnnotator.BL;
+using Swk5.MediaAnnotator.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SWK5.MediaAnnotator.ViewModels
+{
+    public class MediaItemVM : ViewModelBase
+    {
+        private MediaItem item;
+
+        public ICommand SaveCommand { get; set; }
+
+        public string Name
+        {
+            get
+            {
+                return this.item.Name;
+            }
+            set
+            {
+                this.item.Name = value;
+            }
+        }
+
+        public string Url
+        {
+            get
+            {
+                return this.item.Url;
+            }
+            set
+            {
+                this.item.Url = value;
+            }
+        }
+
+        public string Annotation
+        {
+            get
+            {
+                return this.item.Annotation;
+            }
+            set
+            {
+                this.item.Annotation = value;
+                RaisePropertyChangedEvent();
+            }
+        }
+
+        public MediaItemVM(MediaItem item, IMediaManager mediaManager)
+        {
+            this.item = item;
+            this.SaveCommand = new RelayCommand(o => { mediaManager.UpdateAnnotation(this.item); });
+        }
+    }
+
+}
